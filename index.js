75942b0b38ace7b1efc1787527dfa0e76661e99a
infantier <vasyalukin88@gmail.com>
2014-10-04 10:43:55 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!')
-})
+    var fs = require('fs');
+    var content = fs.ReadFileSync('index.html', 'utf-8');
+    response.send(content);
+  });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
